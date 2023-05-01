910d3520f344a0edd478c11d355a73a871fe3ae1
mohammadtk1994 <mohammadtavakolikia66@gmail.com>
2023-05-01 10:11:02 +0330

    Handle games with no image

--- src/components/ColorModeSwitch.tsx
+++ src/components/ColorModeSwitch.tsx
@@ -1,10 +1,11 @@
-import { HStack, Switch, useColorMode } from '@chakra-ui/react'
+import { HStack, Switch, Text, useColorMode } from '@chakra-ui/react'
 
 const ColorModeSwitch = () => {
     const { colorMode, toggleColorMode } = useColorMode()
 
     return (
         <HStack>
+            <Text>Dark Mode</Text>
             <Switch colorScheme='green' isChecked={colorMode === "dark"} onChange={toggleColorMode} />
         </HStack>
     )
